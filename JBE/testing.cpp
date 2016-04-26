ae0b09eeca7dbf846f057e6af21d17112c911a20
Jaime Borondo <jaime.borondo@gmail.com>
2016-04-26 18:04:36 +0200

    Test scenario for full screen

--- JBE/testing.cpp
+++ JBE/testing.cpp
@@ -15,26 +15,18 @@ int main(int argc, char* args[])
 	WindowManager::Initialize("Engine test", 1280, 720);
 	Input::Init();
 
-	std::vector<unsigned> controllers = Input::GetActiveControllers();
-
 	bool quit = false;
+	bool fs = false;
 
 	while (!quit) 
 	{
 		WindowManager::Update();
 		Input::Update();
 		
-		std::vector<unsigned> controllers_t = Input::GetActiveControllers();
-		
-		if (controllers_t.size() != controllers.size()) 
+		if (Input::IsKeyTriggered(SDL_SCANCODE_F))
 		{
-			std::cout << "A controller was ";
-			if (controllers_t.size() > controllers.size())
-				std::cout << "connected." << std::endl;
-			else
-				std::cout << "disconnected." << std::endl;
-
-			controllers = controllers_t;
+			fs = !fs;
+			WindowManager::SetFullscreen(fs);
 		}
 
 
